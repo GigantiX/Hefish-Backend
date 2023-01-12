b5d3d78bc8451635653db2829137f06cf3f93131
AxeLabs <axel.ganendra157@gmail.com>
2023-01-12 17:35:00 +0700

    fix db connect

--- src/model/dbconnect.js
+++ src/model/dbconnect.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
-
-const db = mysql.createPool({
+var options = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
-});
+    database: process.env.DB_DATABASE,
+}
+
+const db = mysql.createPool(options);
+console.log(options);
+
 
 exports.db = db;
