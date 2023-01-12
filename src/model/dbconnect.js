4425c56569c0dc77e5ee563cc9bd0499c8877478
AxeLabs <axel.ganendra157@gmail.com>
2023-01-12 21:06:31 +0700

    sql opt set

--- src/model/dbconnect.js
+++ src/model/dbconnect.js
@@ -4,6 +4,10 @@ var options = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
 }
 const db = mysql.createPool(options);
 console.log(options);
